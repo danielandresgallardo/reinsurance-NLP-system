38860e86da0295d9d91d05894b1f2cfdb67c2b33
danielandresgallardo <daniel-gallardo@live.com>
2023-09-12 14:50:11 +0800

    Compare article date to database. Code clean up

--- gr_crawler.py
+++ gr_crawler.py
@@ -1,39 +1,52 @@
 import fetch_and_parse_html
 
-URL = 'https://www.globalreinsurance.com/sections/news'
+def retrieve_data_from_article(url_element, last_date):
 
-soup = fetch_and_parse_html.run(URL)
-
-results = soup.find(id="news_5792")
-
-more_news_URL = results.find("p", class_="more").find('a')['href']
-
-news_elements = results.find_all("div", class_="subSleeve")
-
-for news_element in news_elements:
-    title_element = news_element.find("a")
-    intro_element = news_element.find("p", class_="intro")
-    url_element = title_element['href']
-    print("Title: ", title_element.text.strip())
-    print("Intro: ", intro_element.text.strip())
-    print("URL: ", url_element)
-
+    #print("URL: ", url_element)
+    
     soup = fetch_and_parse_html.run(url_element)
 
-    results = soup.find(id="wrapper_sleeve")
+    #date format = YYYYMMDDHHMMSS
+    article_date = soup.find("p", class_="byline meta").text
+    #Remove characters except digits, convert to int
+    article_date = int(''.join(filter(str.isdigit, article_date.strip("\n"))))
+    #Convert date to Taiwan timezone
+    article_date_tw = article_date//10000 - article_date%10000 + 800
+    print("Date: ",article_date_tw)
 
-    article_date = results.find("p", class_="byline meta").text
-    print("Date: ", article_date.strip("\n"))
-    article_content = results.find("div", class_="storytext")
+    #If article is older than data on database
+    if article_date_tw < last_date:
+        return 1
+    
+    article_title = soup.find("div", class_="story_title").text
+    print("Title: ", article_title.strip("\n"))
+
+    article_intro = soup.find("div", class_="standfirst").text
+    #print("Intro: ", article_intro)
+
+    article_content = soup.find("div", class_="storytext")
     article_content_elements = article_content.find_all("p")
-    print("Content: ")
-    for article_content_element in article_content_elements:
-        print(article_content_element.text)
-    print()
+    #print("Content: ")
+    #for article_content_element in article_content_elements:
+        #print(article_content_element.text)
+    #print()
+    return 0
 
-print(more_news_URL)
+def update_news(last_date):
+    
+    URL = 'https://www.globalreinsurance.com/news/5792.more?navcode=1817'
 
-soup = fetch_and_parse_html.run(more_news_URL)
+    up_to_date = False
 
-news_elements = soup.find_all("div", class_="listBlocks")
-print(news_elements)
+    while not up_to_date:
+        soup = fetch_and_parse_html.run(URL)
+
+        URL = soup.find("a", class_="next")['href']
+
+        news_elements = soup.find_all("div", class_="storyDetails")
+
+        for news_element in news_elements:
+            url_element = news_element.find("h3").find("a")['href']
+            if retrieve_data_from_article(url_element, last_date) == 1:
+                up_to_date = True
+                break
