ba3d5605b1f3ee59b9adc65a3e1b599f7a986bbb
danielandresgallardo <daniel-gallardo@live.com>
2023-09-14 15:15:00 +0800

    Datetime, last date from database, delete old news

--- updater.py
+++ updater.py
@@ -1,7 +1,27 @@
-import gr_crawler
+import rn_crawler
+import db_connector
+from datetime import datetime
+import pytz
+from dateutil.relativedelta import relativedelta
+
+#for testing purposes
+db_connector.delete_all_data()
+
+# datetime object containing current date and time
+now = datetime.utcnow().replace(tzinfo=pytz.utc)
 
 #get last date of article on database
+last_date = db_connector.fetch_last_date()
+if last_date != None:
+    last_date = datetime.strptime(str(last_date), '%Y-%m-%dT%H:%M:%S%z')
+else:
+    last_date = now - relativedelta(years=1)
+    #date_temp = "2023-09-10T15:43:00+0100"
+    #last_date = datetime.strptime(date_temp, '%Y-%m-%dT%H:%M:%S%z')
+print(last_date)
 
 #delete articles that are over a year old from database
+db_connector.delete_old_articles(now - relativedelta(years=1))
 
-gr_crawler.update_news(20230910055600)
+#gr_crawler2.update_news(last_date)
+rn_crawler.update_news(last_date)
