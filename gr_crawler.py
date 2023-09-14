ee96ec1f770aaf9db90fe07e6a9c81c17067577c
danielandresgallardo <daniel-gallardo@live.com>
2023-09-14 14:59:30 +0800

    Convert all date to datetime. Added googletrans.

--- gr_crawler.py
+++ gr_crawler.py
@@ -1,32 +1,48 @@
 import fetch_and_parse_html
 import db_connector
+from datetime import datetime
+from googletrans import Translator
 
 def retrieve_data_from_article(url_element, last_date):
     soup = fetch_and_parse_html.run(url_element)
 
-    #date format = YYYYMMDDHHMMSS
-    article_date = soup.find("p", class_="byline meta").text
-    #Remove characters except digits, convert to int
-    article_date = int(''.join(filter(str.isdigit, article_date.strip("\n"))))
-    #Convert date to Taiwan timezone
-    article_date_tw = article_date//10000 - article_date%10000 + 800
-    #print("Date: ",article_date_tw)
+    #fetch date from website
+    article_date = soup.find("p", class_="byline meta").text.strip()
 
-    #If article is older than data on database
-    if article_date_tw < last_date:
+    #Convert to datetime
+    article_date = article_date[-25:-3]+article_date[-2:]
+    article_date = datetime.strptime(article_date, '%Y-%m-%dT%H:%M:%S%z')
+
+    #If article is older than data on database, exit
+    if article_date < last_date:
         return 1
     
+    #Convert datetime to string format (%Y-%m-%dT%H:%M:%S%z)
+    article_date = article_date.strftime("%Y-%m-%dT%H:%M:%S%z")
+
+    #Fetch title from website
     article_title = soup.find("div", class_="story_title").text
 
-    article_intro = soup.find("div", class_="standfirst").text
+    article_content = soup.find("div", class_="standfirst").text
 
-    article_content = soup.find("div", class_="storytext")
-    article_content_elements = article_content.find_all("p")
-    article_content = ""
+    #Fetch content from website
+    article_temp = soup.find("div", class_="storytext")
+    article_content_elements = article_temp.find_all("p")
     for article_content_element in article_content_elements:
         article_content += article_content_element.text
     
-    db_connector.add_article(article_title, "Glabal Reinsurance", article_date, article_content, url_element)
+    db_connector.add_article(article_title, 0, "Glabal Reinsurance", article_date, article_content, url_element)
+
+
+    translator = Translator()
+
+    translated_title = translator.translate(article_title, dest='zh-tw').text
+
+    translated_content = translator.translate(article_content, dest='zh-tw').text
+
+    db_connector.add_translation(translated_title, translated_content)
+
+    
     return 0
 
 def update_news(last_date):
