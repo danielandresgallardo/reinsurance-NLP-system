b04ed1933b09da2e5c37374af2156e9bbebe41b1
danielandresgallardo <daniel-gallardo@live.com>
2023-09-17 14:47:19 +0800

    Fix float value

--- retrieve_reinsurer_info.py
+++ retrieve_reinsurer_info.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-import db_connector
+import DbUtilities
 
 url = "https://www.reinsurancene.ws/top-50-reinsurance-groups/"
 
@@ -23,17 +23,25 @@ if response.status_code == 200:
             value = value.strip()
             return int(value) if value and value != 'N/A' else None
         
+        def to_float_or_none(value):
+            if value is not None:
+                cleaned_value = value.strip().replace('%', '')
+                if cleaned_value != 'N/A' and cleaned_value != "":
+                    return float(cleaned_value)
+            return None
+
+        
         glnl = to_int_or_none(''.join(filter(str.isdigit, columns[2].text)))
         nlnl = to_int_or_none(''.join(filter(str.isdigit, columns[3].text)))
         gnlo = to_int_or_none(''.join(filter(str.isdigit, columns[4].text)))
         nnlo = to_int_or_none(''.join(filter(str.isdigit, columns[5].text)))
         shareholders_fund = to_int_or_none(''.join(filter(str.isdigit, columns[6].text)))
-        loss_ratio = to_int_or_none(''.join(filter(str.isdigit, columns[7].text)))
-        expense_ratio = to_int_or_none(''.join(filter(str.isdigit, columns[8].text)))
-        combined_ratio = to_int_or_none(''.join(filter(str.isdigit, columns[9].text)))
+        loss_ratio = to_float_or_none(''.join([char for char in columns[7].text if char.isdigit() or char == '.']))
+        expense_ratio = to_float_or_none(''.join([char for char in columns[8].text if char.isdigit() or char == '.']))
+        combined_ratio = to_float_or_none(''.join([char for char in columns[9].text if char.isdigit() or char == '.']))
 
 
-        db_connector.add_reinsurer(ranking, company_name, glnl, nlnl, gnlo, nnlo, shareholders_fund, loss_ratio, expense_ratio, combined_ratio)
+        DbUtilities.add_reinsurer(ranking, company_name, glnl, nlnl, gnlo, nnlo, shareholders_fund, loss_ratio, expense_ratio, combined_ratio)
 
 else:
     print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
