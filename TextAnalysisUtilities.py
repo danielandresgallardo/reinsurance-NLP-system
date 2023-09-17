27d08ac3be136c0ae36484c55fc6e70de5396686
danielandresgallardo <daniel-gallardo@live.com>
2023-09-17 14:49:32 +0800

    Added subjectivity. Link reinsurer, cat to article

--- TextAnalysisUtilities.py
+++ TextAnalysisUtilities.py
@@ -5,33 +5,37 @@ from fuzzywuzzy import fuzz
 
 #NLP pipeline
 nlp = spacy.load('en_core_web_sm')
+nlp.add_pipe('spacytextblob')
 
-#Test data
-id = 1
-title = '‘Flight to quality’ provides confident outlook for 2024 – Hannover Re'
-content = '‘Adequate pricing is a prerequisite for us to offer the best possible reinsurance capacity,’ says the reinsurer’s chief executive.During its press briefing in Monte Carlo for the Rendez-Vous de Septembre, Hannover Re said that it anticipated further prices increase in property and casualty reinsurance at 1 January 2024 renewals.The reinsurer explained that this could be attributed to ongoing geopolitical uncertainties, the increasing frequency and severity of natural catastrophe losses and persistently high inflation rates.Loss payments from both primary insurers and reinsurers have risen sharply across this year, it added, with the effect being to maintain a trend towards higher expenditures.Jean-Jacques Henchoz, chief executive at the firm, explained: “We have achieved significantly more adequate prices and conditions during this year’s renewals. However, these improve are not sufficient in view of the still challenging risk situation.“Adequate pricing is a prerequisite for us to offer the best possible reinsurance capacity.Trending upwardsSven Althoff, a member of the reinsurer’s board with responsibility for property and casualty insurance in North America, added that the largest change he had observed in 2023 was “significantly changed retention and pricing levels”.Althoff added that inflation would require Hannover’s customers to handle their own higher exposures, which he said were “underlying in their portfolios”.“The trend for more demand on that side is going to continue and that will meet a market that is still very disciplined on capacity. So from that point of view, we are positive about the outlook here.”However, Klaus Miller, member of the executive board with responsibility for property and casualty reinsurance in Europe, noted: “We have to address issues around climate change with our clients and provide more covers on a parametric basis for under-developed countries.”Rebalancing Henchoz added that, despite its positive outlook on many sectors of the reinsurance market, Hannover Re believed there were “a number of pressure points in the value chain.He explained: “What is different since 2022 is that the logic of the market has changed considerably with a supply-demand imbalance going on.“On the other hand, we went through a number of years of so-called soft markets, meaning that there was enough capacity in the system to sustain the risk appetites [of the market].“This changed dramatically last year – the reality of today’s world is that the price of risk is increasing. Society needs to accept that the price is increasing.”'
+words =['reinsurance', 'insurance', 'group', 'limited', 'mutual', 'company', 'ltd', 'ltd.', 'inc', 'holdings', 'co', 'capital', 'international']
 
 #Sentiment analysis(id, content)
 def sentiment_analysis(content):
 
     #Sentiment analysis pipeline
-    nlp.add_pipe('spacytextblob')
 
     scores = []
+    subjectivity_scores = []
 
     doc = nlp(content)
     #Split article to sentences
     for sent in doc.sents:
         #Sentiment analisis of every sentence
         sentiment = sent._.blob.polarity
+        subjectivity = doc._.blob.subjectivity                        
         sentiment = round(sentiment,2)
 
         #Save to list
         scores.append(sentiment)
+        subjectivity_scores.append(subjectivity)
 
     overall_sentiment = sum(scores) / len(scores)
     overall_sentiment = round(overall_sentiment, 2)
-    return overall_sentiment
+
+
+    overall_subjectivity = sum(subjectivity_scores) / len(subjectivity_scores)
+    overall_subjectivity = round(overall_subjectivity, 2)
+    return (overall_sentiment, overall_subjectivity)
 
 #Named entity recognition(title, content)
 def named_entity_recognition(content):
@@ -46,46 +50,102 @@ def named_entity_recognition(content):
     
     return entities_list
 
-def find_matches(entities, org_names, threshold):
+# Function to remove specified words from text using SpaCy
+def remove_words_from_text(text, words):
+    doc = nlp(text)
+    cleaned_text = " ".join([token.text for token in doc if token.text.lower() not in words])
+    return cleaned_text
+
+
+
+def find_matches(article_id, entities, org_names, threshold):
     for entity in entities:
-        entity_doc = nlp(entity)
-        
-        for id, org_name in org_names:
-            org_doc = nlp(org_name)
-            similarity_score = entity_doc.similarity(org_doc)
-            
+        entity = remove_words_from_text(entity, words)
+
+        for reinsurer_id, org_name in org_names:
+            org_name = remove_words_from_text(org_name, words)
+
+            similarity_score = fuzz.ratio(entity.lower(), org_name.lower())
+
             if similarity_score >= threshold:
+                print("Similarity found: ")
                 print(entity)
-                print(similarity_score)
                 print(org_name)
-                print()
+                DbUtilities.link_reinsurer_to_article(article_id, reinsurer_id)
 
-#Set label for new risk, disasters (list of entites)
+def extract_keywords(title, article_content):
+    # Process the title and article content using spaCy's NLP pipeline
+    doc = nlp(title + "\n" + article_content)
 
-    #Compare keywords of risks and disasters to list of entities
-    #if find match, add tag to article
+    # Initialize a set to store unique keywords
+    keywords = set()
+
+    # Define a set of part-of-speech tags that you want to consider as keywords (e.g., NOUN, ADJ)
+    keyword_pos_tags = {"NOUN", "ADJ"}
+
+    # Iterate through the processed tokens
+    for token in doc:
+        if token.pos_ in keyword_pos_tags:
+            keywords.add(token.text)
+
+    return list(keywords)
+
+def calculate_category_probability(article_keywords, category_keywords_list):
+    # Initialize an empty dictionary to store category probabilities
+    category_probabilities = {}
+
+    # Iterate through each category and its keywords
+    for category_id, category_name, category_keywords in category_keywords_list:
+
+        # Use spaCy to tokenize the keywords for more accurate matching
+        article_keywords_tokens = nlp(" ".join(article_keywords))
+        category_keywords_tokens = nlp(" ".join(category_keywords))
+
+        # Calculate the probability score based on the number of matching tokens
+        matching_tokens = len(set(token.text for token in article_keywords_tokens) & set(token.text for token in category_keywords_tokens))
+        probability = matching_tokens / len(category_keywords_tokens)
+
+        # Store the category probability
+        category_probabilities[category_id] = probability
+
+    return category_probabilities
+
+def select_top_categories(category_probabilities):
+    # Find the maximum probability value
+    max_probability = max(category_probabilities.values())
+
+    # Filter categories with the maximum probability
+    top_categories = [category for category, probability in category_probabilities.items() if probability == max_probability]
+
+    return top_categories
+
+def set_categories(article_id, title, content):
+
+    category_keywords_list = DbUtilities.get_categories()
+    article_keywords = extract_keywords(title, content)
+    category_probabilities = calculate_category_probability(article_keywords, category_keywords_list)
+    top_categories = select_top_categories(category_probabilities)
+    for category_id in top_categories:
+        DbUtilities.link_category_to_article(article_id, category_id)
 
 
 def Analyze_text(id, title, content):
 
-    score = sentiment_analysis(content)
-    if score < 0:
-        label = "Positive"
-    else:
-        label = "Negative"
-    DbUtilities.add_sentiment_analysis(id, label, score)
+    sentiment, subjectivity = sentiment_analysis(content)
+
+    DbUtilities.add_sentiment_analysis(id, sentiment, subjectivity)
 
     ner_list = named_entity_recognition(content)
     org_list = DbUtilities.get_reinsurer_list()
 
-    find_matches(org_list, ner_list)
+    find_matches(id, ner_list, org_list, 70)
+
+    set_categories(id, title, content)
 
 
 #print(sentiment_analysis(content))
 #print(named_entity_recognition(content))
-ner_list = named_entity_recognition(content)
-print(ner_list)
-org_list = DbUtilities.get_reinsurer_list()
-print(org_list)
+#ner_list = named_entity_recognition(content)
+#org_list = DbUtilities.get_reinsurer_list()
 
-find_matches(ner_list, org_list, 0.7)
+#find_matches(id, ner_list, org_list, 70)
