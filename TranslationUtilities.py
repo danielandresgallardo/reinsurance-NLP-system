63adea7826e89b54ba1329a5f63a06bbb920f3b8
danielandresgallardo <daniel-gallardo@live.com>
2023-09-18 09:39:54 +0800

    Added translate_to_english()

--- TranslationUtilities.py
+++ TranslationUtilities.py
@@ -10,3 +10,13 @@ def translate_and_upload(id, title, content):
     translated_content = translator.translate(content, dest='zh-tw').text
 
     DbUtilities.add_translation(id, translated_title, translated_content)
+
+def translate_to_english(title, content):
+
+    translator = Translator()
+
+    translated_title = translator.translate(title, dest='en').text
+
+    translated_content = translator.translate(content, dest='en').text
+
+    return(translated_title, translated_content)
