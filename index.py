9d4d0a8e092b52ff9bc97194b5db69c626c0c031
danielandresgallardo <daniel-gallardo@live.com>
2023-09-18 11:19:21 +0800

    Close database connection after every query

--- index.py
+++ index.py
@@ -5,14 +5,14 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
-# Configure the database connection
-db = mysql.connector.connect(
-  host="localhost",
-  user= "root",
-  password= "rootroot",
-  database= "reinsurance_nlp_db",
-)
-cursor = db.cursor()
+# Define a function to create a database connection
+def create_db_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="rootroot",
+        database="reinsurance_nlp_db",
+    )
 
 # Define a route for the homepage with top navigation bar
 @app.route('/')
@@ -23,90 +23,111 @@ def index():
 @app.route('/news')
 @app.route('/news/<category>')
 def news(category=None):
-    if category:
-        cursor.execute("SELECT article_id FROM article_categories WHERE category_id = %s", (category,))
-        articles_id = cursor.fetchall()
-        placeholders = ', '.join(['%s'] * len(articles_id))
-        cursor.execute("SELECT article_id, title FROM translated_articles WHERE article_id IN ({placeholders})",articles_id)
-    else:
-        cursor.execute("SELECT article_id, title FROM translated_articles")
-    articles = cursor.fetchall()
+    try:
+        db = create_db_connection()
+        cursor = db.cursor()
+
+        if category:
+            cursor.execute("SELECT article_id FROM article_categories WHERE category_id = %s", (category,))
+            articles_id = cursor.fetchall()
+            placeholders = ', '.join(['%s'] * len(articles_id))
+            cursor.execute("SELECT article_id, title FROM translated_articles WHERE article_id IN ({placeholders})",articles_id)
+        else:
+            cursor.execute("SELECT article_id, title FROM translated_articles")
+        articles = cursor.fetchall()
+    finally:
+        cursor.close()
+        db.close()
 
     return render_template('news.html', articles=articles, selected_category=category)
 
 @app.route('/article/<int:article_id>')
 def article(article_id):
-    # Fetch the list of reinsurer IDs associated with the article
-    cursor.execute("SELECT reinsurer_id FROM reinsurer_news_article_relationship WHERE article_id = %s", (article_id,))
-    reinsurer_id_rows = cursor.fetchall()
+    try:
+        db = create_db_connection()
+        cursor = db.cursor()
 
-    reinsurer_ids = []
-    for row in reinsurer_id_rows:
-        reinsurer_ids.append(row)
-    # Fetch reinsurer data based on the list of IDs
-    reinsurer_data = []
-    if reinsurer_ids != "":
-        for reinsurer_id in reinsurer_ids:
-            cursor.execute("SELECT * FROM reinsurer_info WHERE id = %s", (reinsurer_id[0],))
-            reinsurer_info = cursor.fetchone()
-            if reinsurer_info:
-                reinsurer_data.append(reinsurer_info)
-        print(reinsurer_data)
-        # Fetch the article data
-        cursor.execute("SELECT * FROM news_articles WHERE id = %s", (article_id,))
-        article = list(cursor.fetchone())
-        print(article)
-    
-    cursor.execute("SELECT sentiment_score, subjectivity_score FROM sentiment_analysis WHERE article_id = %s", (article_id,))
-    sentiment_data = cursor.fetchall()
-    sentiment_data = sentiment_data[0]
-    sentiment_data = [round(sentiment_data[0]*100),'',round(sentiment_data[1]*100)]
+        # Fetch the list of reinsurer IDs associated with the article
+        cursor.execute("SELECT reinsurer_id FROM reinsurer_news_article_relationship WHERE article_id = %s", (article_id,))
+        reinsurer_id_rows = cursor.fetchall()
 
-    if sentiment_data[0] > 25:
-        sentiment_data[1] = 'Positive'
-    elif sentiment_data[0] < -25:
-        sentiment_data[1] = 'Negative'
-    else:
-        sentiment_data[1] = 'Neutral'
-    
+        reinsurer_ids = []
+        for row in reinsurer_id_rows:
+            reinsurer_ids.append(row)
+        # Fetch reinsurer data based on the list of IDs
+        reinsurer_data = []
+        if reinsurer_ids != "":
+            for reinsurer_id in reinsurer_ids:
+                cursor.execute("SELECT * FROM reinsurer_info WHERE id = %s", (reinsurer_id[0],))
+                reinsurer_info = cursor.fetchone()
+                if reinsurer_info:
+                    reinsurer_data.append(reinsurer_info)
+            print(reinsurer_data)
+            # Fetch the article data
+            cursor.execute("SELECT * FROM news_articles WHERE id = %s", (article_id,))
+            article = list(cursor.fetchone())
+            print(article)
+        
+        cursor.execute("SELECT sentiment_score, subjectivity_score FROM sentiment_analysis WHERE article_id = %s", (article_id,))
+        sentiment_data = cursor.fetchall()
+        sentiment_data = sentiment_data[0]
+        sentiment_data = [round(sentiment_data[0]*100),'',round(sentiment_data[1]*100)]
 
-    cursor.execute("SELECT title, content FROM translated_articles WHERE article_id = %s", (article_id,))
-    translated = cursor.fetchone()
+        if sentiment_data[0] > 25:
+            sentiment_data[1] = 'Positive'
+        elif sentiment_data[0] < -25:
+            sentiment_data[1] = 'Negative'
+        else:
+            sentiment_data[1] = 'Neutral'
+        
 
-    article[1] = translated[0]
-    article[5] = translated[1]
+        cursor.execute("SELECT title, content FROM translated_articles WHERE article_id = %s", (article_id,))
+        translated = cursor.fetchone()
+
+        article[1] = translated[0]
+        article[5] = translated[1]
+    finally:
+        cursor.close()
+        db.close()
 
     return render_template('article.html', article=article, reinsurer_data=reinsurer_data, sentiment_data=sentiment_data)
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
-    if request.method == 'POST':
-        # Retrieve search parameters from the form
-        category = request.form.get('category')
-        reinsurer = request.form.get('reinsurer')
-        source = request.form.get('source')  # Get the source parameter
+    try:
+        db = create_db_connection()
+        cursor = db.cursor()
 
-        # Create and execute SQL query based on search parameters
-        query = "SELECT article_id, title FROM translated_articles WHERE 1=1"
-        if category:
-            query += f" AND article_id IN (SELECT article_id FROM article_categories WHERE category_id = '{category}')"
-        if reinsurer:
-            query += f" AND article_id IN (SELECT article_id FROM reinsurer_news_article_relationship WHERE reinsurer_id = '{reinsurer}')"
-        if source:
-            query += f" AND article_id IN (SELECT id FROM news_articles WHERE source = '{source}')"
+        if request.method == 'POST':
+            # Retrieve search parameters from the form
+            category = request.form.get('category')
+            reinsurer = request.form.get('reinsurer')
+            source = request.form.get('source')  # Get the source parameter
 
-        cursor.execute(query)
-        search_results = cursor.fetchall()
-        return render_template('search_results.html', results=search_results, category=category, reinsurer=reinsurer, source=source)
+            # Create and execute SQL query based on search parameters
+            query = "SELECT article_id, title FROM translated_articles WHERE 1=1"
+            if category:
+                query += f" AND article_id IN (SELECT article_id FROM article_categories WHERE category_id = '{category}')"
+            if reinsurer:
+                query += f" AND article_id IN (SELECT article_id FROM reinsurer_news_article_relationship WHERE reinsurer_id = '{reinsurer}')"
+            if source:
+                query += f" AND article_id IN (SELECT id FROM news_articles WHERE source = '{source}')"
 
-    # Retrieve the list of available categories
-    cursor.execute("SELECT id, name FROM categories_info")
-    categories = cursor.fetchall()
+            cursor.execute(query)
+            search_results = cursor.fetchall()
+            return render_template('search_results.html', results=search_results, category=category, reinsurer=reinsurer, source=source)
 
-    # Retrieve the list of available reinsurers
-    cursor.execute("SELECT id, name FROM reinsurer_info")
-    reinsurers = cursor.fetchall()
+        # Retrieve the list of available categories
+        cursor.execute("SELECT id, name FROM categories_info")
+        categories = cursor.fetchall()
 
+        # Retrieve the list of available reinsurers
+        cursor.execute("SELECT id, name FROM reinsurer_info")
+        reinsurers = cursor.fetchall()
+
+    finally:
+        cursor.close()
+        db.close()
     return render_template('search.html', categories=categories, reinsurers=reinsurers)
 
 
