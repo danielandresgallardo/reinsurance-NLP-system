84bc2d37b3b9252814a83559ac9a4e007a6f7992
danielandresgallardo <daniel-gallardo@live.com>
2023-09-18 09:44:08 +0800

    Added rfi

--- updater.py
+++ updater.py
@@ -1,27 +1,32 @@
+import gr_crawler
 import rn_crawler
-import db_connector
+import rfi_crawler
+import DbUtilities
 from datetime import datetime
 import pytz
 from dateutil.relativedelta import relativedelta
 
 #for testing purposes
-db_connector.delete_all_data()
+#DbUtilities.delete_all_articles()
 
 # datetime object containing current date and time
 now = datetime.utcnow().replace(tzinfo=pytz.utc)
 
+#delete articles that are over a year old from database
+DbUtilities.delete_old_articles(now - relativedelta(years=1))
+
 #get last date of article on database
-last_date = db_connector.fetch_last_date()
+last_date = DbUtilities.fetch_last_date()
 if last_date != None:
     last_date = datetime.strptime(str(last_date), '%Y-%m-%dT%H:%M:%S%z')
 else:
-    last_date = now - relativedelta(years=1)
-    #date_temp = "2023-09-10T15:43:00+0100"
+    #Set last date to 1 year ago, for testing purposes just set it to 1 month ago
+    #last_date = now - relativedelta(years=1)
+    last_date = now - relativedelta(months=1)
+    #date_temp = "2023-09-10T15:43:00+0800"
     #last_date = datetime.strptime(date_temp, '%Y-%m-%dT%H:%M:%S%z')
 print(last_date)
 
-#delete articles that are over a year old from database
-db_connector.delete_old_articles(now - relativedelta(years=1))
-
-#gr_crawler2.update_news(last_date)
+gr_crawler.update_news(last_date)
 rn_crawler.update_news(last_date)
+rfi_crawler.update_news(last_date)
